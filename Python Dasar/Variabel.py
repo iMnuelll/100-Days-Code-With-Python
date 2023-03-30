3a9d115aa1ac479e273d59a1be541c29e28e8b0b
IMANUEL UNEPUTTY <imanueluneputty121103@gmail.com>
2023-03-30 20:17:59 +0700

    ThisIsTheFirst

---
+++ Python Dasar/Variabel.py
@@ -0,0 +1,11 @@
+# a adalah Variabel, 10 adalah Value, = adalah Assignments
+# Variable adalah tempat menyimpan data
+# Dari segi penamaan, Variabel tidak harus satu huruf. Tidak boleh ada spasi, angka di depan
+
+a = 10
+x = 5
+panjang = 1000
+
+print("Nilai a = ", a)
+print("Nilai x = ", x)
+print("Nilai panjang = ", panjang)
