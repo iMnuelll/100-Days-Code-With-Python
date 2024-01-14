e8582d28c49bed613cf4fe0154981a69d39a4665
iMnuelll <107970871+iMnuelll@users.noreply.github.com>
2024-01-14 17:25:59 +0800

    Day 3.2

--- Day3.py
+++ Day3.py
@@ -10,47 +10,79 @@ if height > 120 :
     if age < 12 :
        bill = 5
        print(f"Child tickets are ${bill}")
-    elif age >= 12 and age <= 18 :
+    elif age >= 12 and age < 18 :
        bill = 7
        print(f"Youth tickets are ${bill}")
+    elif age >= 45 and age <= 55 :
+       bill = 0
+       print("Everything is going to be ok. Have a free ride on us!!!")
     else :
        bill = 12
        print(f"Adults tickets are ${bill}")
     want_photos = input("Would you like to take photos? (Y/N) : ")
     if want_photos == "Y" :
        bill += 3
+    else :
+       bill += 0
     print(f"Your tickets are ${bill}")
 else :
     print("Sorry, you can't ride the rollercoaster")
 
-# Mengecek angka ganjil dan genap
-# Which number do you want to check?
-print("\nMengecek angka ganjil dan genap")
-number = int(input("Masukkan angkanya : "))
-# 🚨 Don't change the code above 👆
+# # Mengecek angka ganjil dan genap
+# # Which number do you want to check?
+# print("\nMengecek angka ganjil dan genap")
+# number = int(input("Masukkan angkanya : "))
+# # 🚨 Don't change the code above 👆
 
-# Write your code below this line 👇
-if number % 2 == 1 :
-  print("This is an odd number.")
-else :
-  print("This is an even number.")
+# # Write your code below this line 👇
+# if number % 2 == 1 :
+#   print("This is an odd number.")
+# else :
+#   print("This is an even number.")
 
-# BMI 2.0
-# Enter your height in meters e.g., 1.55
-height = float(input())
-# Enter your weight in kilograms e.g., 72
-weight = int(input())
-# 🚨 Don't change the code above 👆
+# # BMI 2.0
+# # Enter your height in meters e.g., 1.55
+# height = float(input())
+# # Enter your weight in kilograms e.g., 72
+# weight = int(input())
+# # 🚨 Don't change the code above 👆
 
-#Write your code below this line 👇
-bmi_calculation = weight / height**2
-if bmi_calculation < 18.5 :
-  print (f"Your BMI is {bmi_calculation}, you are underweight.")
-elif bmi_calculation > 18.5 and bmi_calculation < 25 :
-  print (f"Your BMI is {bmi_calculation}, you have a normal weight.")
-elif bmi_calculation >= 25 and bmi_calculation < 30 :
-  print(f"Your BMI is {bmi_calculation}, you are slightly overweight.")
-elif bmi_calculation > 30 and bmi_calculation < 35 :
-  print(f"Your BMI is {bmi_calculation}, you are obese.")
-else :
-  print(f"Your BMI is {bmi_calculation}, you are clinically obese.")
+# #Write your code below this line 👇
+# bmi_calculation = weight / height**2
+# if bmi_calculation < 18.5 :
+#   print (f"Your BMI is {bmi_calculation}, you are underweight.")
+# elif bmi_calculation > 18.5 and bmi_calculation < 25 :
+#   print (f"Your BMI is {bmi_calculation}, you have a normal weight.")
+# elif bmi_calculation >= 25 and bmi_calculation < 30 :
+#   print(f"Your BMI is {bmi_calculation}, you are slightly overweight.")
+# elif bmi_calculation > 30 and bmi_calculation < 35 :
+#   print(f"Your BMI is {bmi_calculation}, you are obese.")
+# else :
+#   print(f"Your BMI is {bmi_calculation}, you are clinically obese.")
+
+# # Pizza Order
+# print("Thank you for choosing Python Pizza Deliveries!")
+# size = input()  # What size pizza do you want? "S", "M", or "L"
+# add_pepperoni = input()  # Do you want pepperoni? "Y" or "N"
+# extra_cheese = input()  # Do you want extra cheese? "Y" or "N"
+
+# # Your code below this line 👇
+# bill = 0
+# if size == "S":
+#   bill += 15
+# elif size == "M":
+#   bill += 20
+# else:
+#   bill += 25
+
+# if add_pepperoni == "Y":
+#   if size == "S":
+#     bill += 2
+#   else:
+#     bill += 3
+
+# if extra_cheese == "Y":
+#   bill += 1
+
+# print(f"Your final bill is: ${bill}.")
+# # Pizza Order
