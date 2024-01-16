5b8c57f8b1b2644b03accf4a40e37110f19f3d2c
iMnuelll <107970871+iMnuelll@users.noreply.github.com>
2024-01-16 13:20:15 +0800

    Day3 Done

--- Day3.py
+++ Day3.py
@@ -114,3 +114,24 @@ elif (score >= 40) and (score <= 50):
 else:
   print(f"Your score is {score}.")
 # Love Calculator
+  
+# Program Mengecek Tahun Kabisat
+# Which year do you want to check?
+year = int(input("Masukkan tahun : "))
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+decision1 = "Leap year"
+decision2 = "Not leap year"
+
+if year % 4 == 0 :
+  if year % 100 != 0 :
+    print(decision1)
+  elif year % 100 == 0 :
+    if year % 400 == 0 :
+      print(decision1)
+    else :
+      print(decision2)
+elif year % 4 != 0 :
+  print(decision2)
+# Program Mengecek Tahun Kabisat
