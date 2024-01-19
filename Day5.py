35dcfc00a58eb2cd5fa686f14c60fa0bb533e3f5
iMnuelll <107970871+iMnuelll@users.noreply.github.com>
2024-01-19 22:21:31 +0800

    Day 5.1 Progress

--- Day5.py
+++ Day5.py
@@ -26,3 +26,26 @@ print (f"number of students = {total_students}")
 average = total_heights / total_students
 print(f"average height = {round(average)}")
 
+# Highest Score
+# Input a list of student scores
+student_scores = input().split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+
+# Write your code below this row 👇
+highest_score = 0
+for score in student_scores :
+  if score > highest_score :
+    highest_score = score
+print(f"The highest score in the class is: {highest_score}")
+
+# Adding Even Number
+target = int(input()) # Enter a number between 0 and 1000
+# 🚨 Do not change the code above ☝️
+
+# Write your code here 👇
+jumlah = 0
+for i in range (1, target+1) :
+    if i % 2 == 0 :
+        jumlah += i
+print(jumlah)
