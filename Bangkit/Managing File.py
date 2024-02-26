4c9792152b180da619034905d762385ece805229
iMnuelll <107970871+iMnuelll@users.noreply.github.com>
2024-02-26 10:51:38 +0800

    yep

---
+++ Bangkit/Managing File.py
@@ -0,0 +1,16 @@
+import os
+
+dir = "./Basic Python/100-Days-Code-with-Python"
+
+for name in os.listdir(dir) :
+    """
+    os.listdir() -> List the directories and files in the desired directory
+    """
+    fullname = os.path.join(dir, name)
+    """
+    os.path.join() -> Combines the directory path that has been initialized in the dir variable with the directory/file name in the name variable
+    """
+    if os.path.isdir(fullname):
+         print("{} is a directory".format(fullname))
+    else:
+         print("{} is a file".format(fullname))
---
+++ Bangkit/idk.py
@@ -0,0 +1,5 @@
+def f(x) :
+    return 3.14 * x * 2
+x = 7
+y = f(x)
+print(y)
