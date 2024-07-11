6e1d6cf44737bb2887ec6211b1ef5c693c7b90d4
iMnuelll <107970871+iMnuelll@users.noreply.github.com>
2024-07-11 10:28:51 +0800

    day18_finish

--- 100-Days-Code-with-Python/Day18/Hirst Painting/main.py
+++ 100-Days-Code-with-Python/Day18/Hirst Painting/main.py
@@ -1,13 +1,30 @@
-import colorgram
+import turtle as turtle
+import random
 
-rgb_colors = []
-colors = colorgram.extract('100-Days-Code-with-Python\Day18\Hirst Painting\image.jpg', 30)
+number_of_dots = int(input("How many dots you want to create? : "))
 
-for color in colors :
-    r = color.rgb.r
-    g = color.rgb.g
-    b = color.rgb.b
-    new_color = (r, g, b)
-    rgb_colors.append(new_color)
+turtle.colormode(255)
+tim = turtle.Turtle()
+tim.penup()
+tim.hideturtle()  
+tim.speed("fastest")
 
-print(rgb_colors)
+color_list = [(252, 250, 247), (253, 247, 249), (237, 251, 245), (249, 228, 17), (213, 13, 9), (198, 12, 35), (231, 228, 5), (197, 69, 20), (33, 90, 188), (43, 212, 71), (234, 148, 40), (33, 30, 152), (16, 22, 55), (66, 9, 49), (240, 245, 251), (244, 39, 149), (65, 202, 229), (14, 205, 222), (63, 21, 10), (224, 19, 111), (229, 165, 8), (15, 154, 22), (245, 58, 16), (98, 75, 9), (248, 11, 9), (222, 140, 203), (68, 240, 161), (10, 97, 62), (5, 38, 33), (68, 219, 155)]
+
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+
+for dot_count in range (1, number_of_dots + 1) :
+    tim.dot(10, random.choice(color_list))
+    tim.forward(50)
+    
+    if dot_count % 10 == 0 :
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)  
+
+screen = turtle.Screen()
+screen.exitonclick()
