a7580720905ced95fe0988256752d96548466824
iMnuelll <imanueluneputty121103@gmail.com>
2024-09-03 18:59:50 +0700

    idk

--- 100-Days-Code-with-Python/Day22/main.py
+++ 100-Days-Code-with-Python/Day22/main.py
@@ -1,53 +1,9 @@
-from turtle import Screen
-from paddle import Paddle
-from ball import Ball
-import time
-
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+from turtle import Turtle, Screen
 
 screen = Screen()
-right_paddle = Paddle(370, 0)
-left_paddle = Paddle(-370, 0)
-ball = Ball()
 
-screen.bgcolor("black")
-screen.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
-screen.title("PONG")
-screen.tracer(0)
-
-screen.listen()
-# Right paddle controller
-screen.onkey(right_paddle.move_up, "Up")
-screen.onkey(right_paddle.move_down, "Down")
-
-# Left paddle controller
-screen.onkey(left_paddle.move_up, "w")
-screen.onkey(left_paddle.move_down, "s")
-
-game_is_on = True
-
-while game_is_on:
-    time.sleep(0.03)
-    screen.update()
-    ball.move()
-    
-    # Detect collision with top or bottom wall
-    if ball.ycor() > (SCREEN_HEIGHT / 2 - 20) or ball.ycor() < -(SCREEN_HEIGHT / 2 - 20):
-        ball.bounce_y()
-    
-    # Detect collision with left or right wall
-    if ball.xcor() > (SCREEN_WIDTH / 2) or ball.xcor() < -(SCREEN_WIDTH / 2):
-        ball.bounce_x()
-        
-    
-    # Detect collision with right paddle
-    if ball.distance(right_paddle) < 50 and ball.xcor() > 340 :
-        ball.bounce_x()
-    
-    # Detect collision with left paddle
-    if ball.distance(left_paddle) < 50 and ball.xcor() < -340 :
-        ball.bounce_x()
-
+screen.setup(width=800, height= 600)
+screen.bgcolor('black')
+screen.title("Ping Pong")
 
 screen.exitonclick()
