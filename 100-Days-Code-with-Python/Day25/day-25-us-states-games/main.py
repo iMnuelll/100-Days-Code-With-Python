62c0c91818d1e0d90ebfc716b92c84f3982f44f5
iMnuelll <imanueluneputty121103@gmail.com>
2024-09-06 22:43:13 +0700

    day25_documentation

--- 100-Days-Code-with-Python/Day25/day-25-us-states-games/main.py
+++ 100-Days-Code-with-Python/Day25/day-25-us-states-games/main.py
@@ -1,33 +1,39 @@
 import turtle
 import pandas as pd
 
-# Setup the screen
+"""Setup the screen"""
 screen = turtle.Screen()
 screen.title("U.S. State Game")
 image = 'blank_states_img.gif'
 screen.addshape(image)
 turtle.shape(image)
 
-# Read the data
+"""Read the data"""
 data = pd.read_csv("50_states.csv")
+
+"""Make the series of the data into a list"""
 state_list = data['state'].to_list()
 x_cor = data['x'].to_list()
 y_cor = data['y'].to_list()
 
-# Create a turtle for writing the state names
+"""Create a turtle for writing the state names"""
 writer = turtle.Turtle()
 writer.penup()
 writer.hideturtle()
 
+
 def screen_answer():
-    """Get the player's answer from the screen input."""
+    """The screen_answer function is designed to prompt the user to input a state name using a pop-up input box. The prompt displays the number of correctly guessed states so far, out of the total number of states. The user’s input is automatically converted to title case (i.e., capitalizing the first letter of each word). The function then returns the user's input."""
     answer_state = screen.textinput(title=f"{len(guessed_states)} / {len(state_list)} States Correct", prompt="What's another state's name?").title()
     return answer_state
 
 game_is_on = True
+
+"""List of the correct states guess by the user"""
 guessed_states = []
 
-while len(guessed_states) < 50:  # Game continues until all 50 states are guessed
+""""This while loop continuously prompts the user to guess U.S. state names until they have correctly guessed all 50 states or choose to exit. The guessed states are tracked, and the state name is written at its corresponding geographic location on the screen if guessed correctly. If the player decides to exit by typing "Exit", the loop will terminate and save the list of unguessed states to a CSV file."""
+while len(guessed_states) < 50:
     question = screen_answer()
     
     # If the player wants to exit, break the loop
