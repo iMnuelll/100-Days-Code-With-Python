1afb6de329921fc0e9a6763624f52bab75b06fe4
iMnuelll <imanueluneputty121103@gmail.com>
2024-09-13 13:16:23 +0700

    day29 ui fix

--- 100-Days-Code-with-Python/Day29/main.py
+++ 100-Days-Code-with-Python/Day29/main.py
@@ -67,36 +67,38 @@ def save():
 window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
+window.columnconfigure((0,1,2), weight=1)
+window.rowconfigure((0,1,2), weight=1)
 
 # Set up the logo image on a canvas
 canvas = Canvas(height=200, width=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
-canvas.grid(row=0, column=1)
+canvas.grid(row=0, column=1, columnspan=2, sticky="NSEW")
 
 #Labels for website, email and password
 website_label = Label(text="Website:")
-website_label.grid(row=1, column=0)
+website_label.grid(row=1, column=0, sticky="W")
 email_label = Label(text="Email/Username:")
-email_label.grid(row=2, column=0)
+email_label.grid(row=2, column=0, sticky="W")
 password_label = Label(text="Password:")
-password_label.grid(row=3, column=0)
+password_label.grid(row=3, column=0, sticky="W")
 
 #Entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=40)
+website_entry.grid(row=1, column=1, sticky="W", columnspan=2)
 website_entry.focus() # Automatically focus on the website entry field
-email_entry = Entry(width=35)
-email_entry.grid(row=2, column=1, columnspan=2)
+email_entry = Entry(width=40)
+email_entry.grid(row=2, column=1, sticky="W", columnspan=2)
 email_entry.insert(0, "your_email@gmail.com")
 password_entry = Entry(width=21)
-password_entry.grid(row=3, column=1)
+password_entry.grid(row=3, column=1, sticky="W")
 
 # Buttons to generate a password and save the information.
-generate_password_button = Button(text="Generate Password", command=generate_password)
-generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=36, command=save)
-add_button.grid(row=4, column=1, columnspan=2)
+generate_password_button = Button(width=15, text="Generate Password", command=generate_password)
+generate_password_button.grid(row=3, column=2, sticky='W')
+add_button = Button(text="Add", width=34, command=save)
+add_button.grid(row=4, column=1, columnspan=2, sticky="W")
 
 # Start the Tkinter main even loop.
 window.mainloop()
